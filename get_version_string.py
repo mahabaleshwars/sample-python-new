9e816f179b7b5a15fe48fb4ebcdbba2329a3b3a9
gowridurgad <159780674+gowridurgad@users.noreply.github.com>
2024-10-14 12:20:09 +0530

    Update get_version_string.py

--- get_version_string.py
+++ get_version_string.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+# Add the _vendor directory to sys.path
+vendor_path = os.path.join(os.path.dirname(__file__), 'setuptools', '_vendor')
+if vendor_path not in sys.path:
+    sys.path.append(vendor_path)
+
 from packaging import version
 
 print("Hello world")
