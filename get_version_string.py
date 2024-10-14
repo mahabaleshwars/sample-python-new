7592ced6fc0f318705364d81eb5c8095c6273cca
gowridurgad <159780674+gowridurgad@users.noreply.github.com>
2024-10-14 12:29:54 +0530

    Update get_version_string.py

--- get_version_string.py
+++ get_version_string.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Add the _vendor directory to sys.path
-vendor_path = os.path.join(os.path.dirname(__file__), 'setuptools', '_vendor')
+vendor_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'setuptools', '_vendor'))
 if vendor_path not in sys.path:
     sys.path.append(vendor_path)
 
